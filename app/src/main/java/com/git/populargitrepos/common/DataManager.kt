6cf07aebb737abe0c4d36164f7476e33a9065848
RxSajib <sajibroy206@gmail.com>
2025-02-04 11:37:58 +0600

    add repository api

--- app/src/main/java/com/git/populargitrepos/common/DataManager.kt
+++ app/src/main/java/com/git/populargitrepos/common/DataManager.kt
@@ -3,4 +3,8 @@ package com.git.populargitrepos.common
 object DataManager {
 
     const val BASE_URL = "https://api.github.com/"
+
+    const val SEARCH_REPOSITORY = "Android"
+    const val SHORT = "stars"
+    const val ORDER = "desc"
 }
